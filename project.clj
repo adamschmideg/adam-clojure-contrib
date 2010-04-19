237f8d5a9c44492f36b372bb74e513d69a8a2aa7
Adam Schmideg <adam@schmideg.net>
2010-04-19 15:50:37 +0200

    Use leiningen

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject adam-clojure-contrib "0.9"
+  :description "Additional core utilities that could fit into clojure contrib"
+  :url "http://github.com/adamschmideg/adam-clojure-contrib"
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]])
